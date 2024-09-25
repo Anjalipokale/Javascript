ad2ca5e7ceb50fa4b3269ef5f18f9a16a1fca34a
Anjalipokale <168544411+Anjalipokale@users.noreply.github.com>
2024-09-25 12:53:40 +0530

    Methods File

---
+++ methods.js
@@ -0,0 +1,72 @@
+let D = new Date();
+let monthnumber = D.getMonth();
+switch(monthnumber)
+
+{
+    case 1:
+       console.log("jan")
+        break;
+        
+    case 2:
+        console.log("jan")
+        break;
+
+    case 3:
+        console.log("jan")
+            break;
+
+    case 4:
+        console.log("jan")
+                break;
+
+    case 5:
+        console.log("jan")
+            break;
+
+    case 6:
+        console.log("jan")
+            break;
+
+    case 7:
+        console.log("sep")
+             break;
+
+    case 8:
+        console.log("aug")
+            break;
+
+    case 9:
+        console.log("jan")
+            break;
+
+                                
+    case 10:
+        console.log("jan")
+              break;
+
+default:
+    console.log("not found");
+}
+
+
+  // Using Date Method
+
+/*let D = new Date();
+console.log(D.getDate());
+console.log(D.getMonth());
+console.log(D.getTime());
+console.log(D.getFullYear());
+console.log(D.getDay());
+console.log(D.getHours());
+console.log(D.getMinutes());
+console.log(D.getSeconds());
+console.log(D.getMilliseconds());*/
+
+
+// Print Full Date
+// const d = new Date();
+// console.log(d);
+
+
+
+
